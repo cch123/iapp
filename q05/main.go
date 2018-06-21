2e1f5e96f52e272cdd6c1a4638135640defe722a
Xargin <cao1988228@163.com>
2018-06-21 17:13:54 +0800

    add q05

--- q05/main.go
+++ q05/main.go
@@ -7,27 +7,32 @@ func main() {
 	// 10, 50, 100, 500
 	var valArr = []int{10, 50, 100, 500} // const
 	var curCntArr = []int{0, 0, 0, 0}
-	for !canStop(N, curCntArr, valArr) {
-		push(N, curCntArr, valArr)
-		if calcTotal(valArr, curCntArr) == N {
+	var maxCntArr = []int{15, 15, 10, 2}
+	var counter int
+	for {
+		if calcTotal(valArr, curCntArr) == N && totalCoin(curCntArr) < 15 { //&& totalCoin(curCntArr) < 15 {
 			fmt.Println(curCntArr)
+			counter++
+		}
+
+		if !push(N, curCntArr, valArr, maxCntArr) {
+			break
 		}
 	}
+	fmt.Println(counter)
 }
 
-func push(n int, curCntArr []int, valArr []int) {
-	if curCntArr[0]*valArr[0] < n {
-		curCntArr[0]++
-		return
-	}
-
-	curCntArr[0] = 0
-	for i := 1; i < len(curCntArr); i++ {
-		if curCntArr[i]*valArr[i] < n {
-			curCntArr[i]++
-			return
+func push(n int, curCntArr []int, valArr []int, maxCntArr []int) bool {
+	for idx := 0; idx < len(curCntArr); idx++ {
+		if curCntArr[idx] < maxCntArr[idx] {
+			curCntArr[idx]++
+			for i := 0; i < idx; i++ {
+				curCntArr[i] = 0
+			}
+			return true
 		}
 	}
+	return false
 }
 
 func calcTotal(valArr []int, curCntArr []int) int {
@@ -38,13 +43,10 @@ func calcTotal(valArr []int, curCntArr []int) int {
 	return cnt
 }
 
-func canStop(n int, curCntArr []int, valArr []int) bool {
-	var canStop = true
+func totalCoin(curCntArr []int) int {
+	cnt := 0
 	for i := 0; i < len(curCntArr); i++ {
-		if valArr[i]*curCntArr[i] < n {
-			canStop = false
-			break
-		}
+		cnt += curCntArr[i]
 	}
-	return canStop
+	return cnt
 }
